97dd28133b57c3fe68222c2fd8f263c16306ea31
Adrian Carpenter <adrian@fizzyade.com>
2020-11-16 21:03:11 +0000

    chore: updated copyright comments

--- libs/Ribbon/RibbonComboBox.cpp
+++ libs/Ribbon/RibbonComboBox.cpp
@@ -3,6 +3,8 @@
  *
  * https://github.com/fizzyade
  *
+ * Created by Adrian Carpenter on 15/11/2020.
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
